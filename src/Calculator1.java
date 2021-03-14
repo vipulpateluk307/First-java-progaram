604bd57a2a2fb2c10abce77fbd24c8f4125468b1
unknown <you@example.com>
2021-03-14 18:23:22 +0000

    user Details & calculator

---
+++ src/Calculator1.java
@@ -0,0 +1,31 @@
+public class Calculator1 {
+
+        static int a = 20;
+        static int b = 5;
+        static int total = a + b;
+        static int sub = a-b;
+        static int mul = a*b;
+        static int div = a/b;
+    public static void main(String[] args) {
+            System.out.println("Addition of "+ a +" & "+ b +" = "+ total);
+            System.out.println("Subtraction of "+ a +" & "+ b +" = "+ sub);
+            System.out.println("Multiplication of "+ a +" & "+ b +" = "+ mul);
+            System.out.println("Divided of "+ a +" & "+ b +" = "+ div);
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/UserPersonalDetails.java
@@ -0,0 +1,31 @@
+public class UserPersonalDetails {
+
+
+    static String Name = "Vipul Patel";
+    static String Address = "21, London Road, WD23 4PA";
+    static String Mobile = "0712345678";
+    static String Age = "35";
+    static String Security_code = "35.60";
+    static String Eligible_for_vote = "True";
+    static String Grade = "b";
+    public static void main(String[] args) {
+
+        System.out.println("Name : "+ Name);
+        System.out.println("Address : "+ Address);
+        System.out.println("Mobile : "+ Mobile);
+        System.out.println("Age : "+ Age);
+        System.out.println("Security code : "+ Security_code);
+        System.out.println("Eligible for vote : "+ Eligible_for_vote);
+        System.out.println("Grade : "+ Grade);
+
+
+
+
+
+    }
+
+
+
+
+
+}
